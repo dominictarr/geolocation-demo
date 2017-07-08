757781c62005b7ee5e20b07616d719a72de7f743
Dominic Tarr <dominic.tarr@gmail.com>
2017-07-08 13:32:22 +1200

    use great-circle to calculate distance and heading

--- index.js
+++ index.js
@@ -11,9 +11,24 @@ function flatten (p) {
   return o
 }
 
+var positions = []
+
+pre.textContent = 'waiting for position...'
+
 navigator.geolocation.watchPosition(function (e) {
   console.log(e.coords, e.timestamp)
-  pre.textContent = JSON.stringify(flatten(e), null, 2)
-})
+  position.push(flatten(e))
+  //keep just one minute's worth of locations.
+  if(e.timestamp < Date.now() - 60e3) // one minute
+    positions.shift()
 
+  var movement = position.map(function (_e) {
+    return {
+      distance: GreatCircle.distance(_e.latitude, _e.longitude, e.latitude, e.longitude, 'NM'),
+      heading: GreatCircle.bearing(_e.latitude, _e.longitude, e.latitude, e.longitude)
+    }
+  })
+
+  pre.textContent = JSON.stringify(flatten({current:e, movement: movement}), null, 2)
+})
 
